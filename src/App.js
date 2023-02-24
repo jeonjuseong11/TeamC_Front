fa57d960129ef161a4aec8a68b536be5516270fd
WePung <akgre5@naver.com>
2023-02-24 14:02:27 +0900

    Login
    로그인 폼만 만들어놓은 작업

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Login from './Login';
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,111 @@
+import React
+, { useEffect,
+useState } from "react";
+
+const User = 
+    {id: "test@gmail.com",
+    pw: "A!123456789"
+    }
+
+ const Login = () =>{
+
+    const [id, setId] = useState("");
+    const [pw, setPw] = useState("");
+
+    const [idValid, setIdValid]= useState(false);
+    const [pwValid, setPwValid]= useState(false);
+    const [notAllow, setNotAllow] = useState(true);
+
+
+
+    const handleId = (e) => {
+        setId(e.target.value);
+        const regex =
+        /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+        if(regex.test(id)){
+            setIdValid(true);
+        }else{
+            setIdValid(false);
+        }
+    }
+
+    const handlePw = (e) => {
+        setPw(e.target.value);
+        const regex =
+        /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
+        if(regex.test(pw)){
+            setPwValid(true);
+        }else{
+            setPwValid(false);
+        }
+    }
+
+    useEffect( ()=> {
+        if(idValid && pwValid){
+            setNotAllow(false)
+            return;
+        }
+        setNotAllow(true);
+    }, [idValid, pwValid]);
+
+    const onClickConfirmButton = () =>{
+        if(id === User.id && pw === User.pw){
+            alert('로그인 성공');
+        }else if(id !== User.id && pw === User.pw){
+            alert('아이디를 확인해주세요');
+        }else if(id === User.id && pw !== User.pw){
+            alert('패스워드를 확인해주세요');
+        }else{
+            alert('가입자 정보가 없습니다.');
+        }
+    }
+
+    return(
+        <div className="Page">
+            <div className="title">
+                logo부분
+            </div>
+
+            <div className="contentWrap">
+                <div className="id">
+                    <input type = "text"
+                    value={id}
+                    onChange={handleId}
+                    placeholder="아이디를 입력해주세요"></input>
+                    <div className="errMessgeId"
+                    onChange={handleId}>
+                        {
+                            !idValid && id.length > 0 &&(
+                                <div>올바른 아이디를 입력해주세요</div>
+                            )
+                        }
+                </div>
+                </div>
+                <div className="password">
+                    <input type = "password"
+                    value={pw}
+                    onChange={handlePw}
+                    placeholder="패스워드를 입력해주세요"
+                    ></input>
+                </div>
+                <div className="errMessgePassword"
+                >
+                    {
+                            !pwValid && pw.length > 0 &&(
+                                <div>올바른 패스워드를 입력해주세요</div>
+                            )
+                        }
+                </div>
+            </div>
+            <div className="btnWrap">
+                <button className="loginBtn"
+                disabled = {notAllow}
+                onClick = {onClickConfirmButton}>
+                    로그인
+                </button>
+            </div>
+        </div>
+    )
+ }
+
+ export default Login;
