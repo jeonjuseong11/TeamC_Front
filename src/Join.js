973b7116405fc23aad4cb53bcdd2519612be3ffd
WePung <akgre5@naver.com>
2023-02-28 01:53:16 +0900

    Edit - Join
    회원가입 페이지 수정

--- src/Join.js
+++ src/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./Join.css";
 
 const Join = () => {
@@ -42,7 +42,7 @@ const Join = () => {
     const [pwValid, setPwValid] = useState(false);
     const [pwRewindValid, setPwRewindValid] = useState(false);
     const [nameValid, setNameValid] = useState(false);
-    const [emailValid, setEmaiulValid] = useState(false);
+    const [emailValid, setEmailValid] = useState(false);
     const [sexValid, setSexValid] = useState(false);
     const [ageValid, setAgeValid] = useState(false);
     const [isJoin, setIsJoin] = useState(false);
@@ -54,6 +54,23 @@ const Join = () => {
     const [nameMessage, setNameMessage] = useState("");
     const [emailMessage, setEmailMessage] = useState("");
     const [ageMessage, setAgeMessage] = useState("");
+
+
+    // 아이디 동기 처리
+    useEffect(()=>{
+      const regex = /^[a-zA-z0-9]{4,12}$/
+      if(id.length === 0){
+        setIdMessage();
+      }else{
+        if (regex.test(id)) {
+          setIdValid(true);
+          setIdMessage("사용 가능한 아이디입니다.")
+        } else {
+          setIdValid(false);
+          setIdMessage("사용 불가능한 아이디입니다.")
+        }
+      }
+    },[id])
 
     // 아이디 제한
     const handleId = (e) => {
@@ -74,7 +91,25 @@ const Join = () => {
         }
       };
       
+      // 비밀번호 동기 처리
+      useEffect(()=>{
+        const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,12}$/;
+        if(pw.length === 0){
+        setPwMessage();
+      }else{
+        if (regex.test(pw)) {
+          setPwValid(true);
+          setPwMessage("사용 가능한 비밀번호입니다.")
+        } else {
+          setPwValid(false);
+          setPwMessage("사용 불가능한 비밀번호입니다.")
+        }
+      }
+      },[pw])
+
+
       // 비밀번호 제한
+      useEffect(() => {},[]);
       const handlePw = (e) => {
         setPw(e.target.value);
         const {name, value} = e.target
@@ -92,6 +127,22 @@ const Join = () => {
         }
       };
 
+      // 비밀번호 재확인 동기 처리
+      useEffect(()=>{
+        const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,12}$/;
+        if(pwRewind.length === 0){
+        setPwRewindMessage();
+      }else{
+        if (regex.test(pwRewind) && pwRewind === pw) {
+          setPwRewindValid(true);
+          setPwRewindMessage();
+        } else {
+          setPwRewindValid(false);
+          setPwRewindMessage("비밀번호와 일치하지 않습니다.");
+        }
+      }
+      },[pwRewind])
+
       // 비밀번호 확인 제한
       const handlePwRewind = (e) => {
         setPwRewind(e.target.value);
@@ -100,9 +151,23 @@ const Join = () => {
             setPwRewindMessage();
         } else {
             setPwRewindValid(false);
-                setPwRewindMessage("비밀번호가 일치하지 않습니다");
+            setPwRewindMessage("비밀번호가 일치하지 않습니다");
         }
       };
+
+      // 이름 동기 처리
+      useEffect(()=>{
+        const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,12}$/;
+        if(name.length === 0){
+        setNameMessage();
+      }else{
+        if (regex.test(pw)) {
+          setNameValid(true);
+        } else {
+          setNameValid(false);
+        }
+      }
+      },[name])
     
       // 이름 제한
       const handleName = (e) => {
@@ -121,6 +186,22 @@ const Join = () => {
         }
       };
 
+      // 이메일 동기 처리
+      useEffect(()=>{
+        const regex = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+        if(email.length === 0){
+        setEmailMessage();
+      }else{
+        if (regex.test(email)) {
+          setEmailValid(true);
+          setEmailMessage("사용 가능한 이메일입니다.")
+        } else {
+          setEmailValid(false);
+          setEmailMessage("사용 불가능한 이메일입니다.")
+        }
+      }
+      },[email])
+
       // 이메일 제한
       const handleEmail = (e) => {
         setEmail(e.target.value);
@@ -131,18 +212,49 @@ const Join = () => {
         })
         const regex = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
         if (regex.test(email)) {
-            setEmaiulValid(true);
+            setEmailValid(true);
             setEmailMessage("사용 가능한 이메일입니다.");
         } else {
-            setEmaiulValid(false);
+            setEmailValid(false);
             setEmailMessage("사용 불가능한 이메일입니다.");
         }
       };
 
+// 이메일 동기 처리
+      useEffect(()=>{
+        const regex = /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+        if(email.length === 0){
+        setEmailMessage();
+      }else{
+        if (regex.test(email)) {
+          setEmailValid(true);
+          setEmailMessage("사용 가능한 이메일입니다.")
+        } else {
+          setEmailValid(false);
+          setEmailMessage("사용 불가능한 이메일입니다.")
+        }
+      }
+      },[email])
+
+      // 나이(생년월일) 동기 처리
+      useEffect(()=>{
+        if(age.length === 0){
+          setAgeMessage();
+        }else{
+        if(age.length === 8){
+          setAgeValid(true);
+          setAgeMessage();
+      }else{
+        setAgeValid(false);
+        setAgeMessage("생년월일을 확인해주세요.")
+        }
+      }
+      },[age])
+
       // 나이(생년월일) 제한
       const handleAge = (e) => {
         setAge(e.target.value);
-        if (age.length === 7) {
+        if (age.length === 8) {
             setAgeValid(true);
             const {name, value} = e.target
             setInputs({
@@ -158,38 +270,48 @@ const Join = () => {
 
 
       // 계정 배열에 값 insert
-    const onJoin = () => {
+    const onCreate = () => {
         if(id === "" || pw ==="" || pwRewind === "" || name === "" || email === "" || age ===""){
             alert("공란을 확인해주세요");
-        }else{
-          account = {
-            id,
-            pw,
-            name,
-            email,
-            sex,
-            age
-          }
-          setAccounts([...accounts, account])
-          setInputs({
-            id : "",
-            pw : "",
-            name : "",
-            email : "",
-            sex : "",
-            age: ""
-          })
+        }else{if(!idValid || !pwValid || !pwRewindValid || !nameValid || !emailValid || !ageValid){
+          alert("정확한 정보를 기입해주세요")
+         }else{
+            account = {
+              id,
+              pw,
+              name,
+              email,
+              sex,
+              age
+            }
+            setAccounts([...accounts, account])
+            setInputs({
+              id : "",
+              pw : "",
+              name : "",
+              email : "",
+              sex : "",
+              age: ""
+            })
             alert("성공");
+            setIsJoin(false)
+          }
         }
     }
     
     const onReset = () =>{
       setId("");
+      setIdMessage();
       setPw("");
+      setPwMessage();
       setPwRewind("");
+      setPwRewindMessage();
       setName("");
+      setNameMessage();
       setEmail("");
+      setEmailMessage();
       setAge("");
+      setAgeMessage();
     }
 
     return(
@@ -250,12 +372,16 @@ const Join = () => {
                             <p style={emailValid ? {color:"blue"} : {color:"red"}}
                             >{emailMessage}</p>
                 <p>성별</p>
-                <select
-                        id = "sex"
-                        name="sex">
-                            <option value="Male">남</option>
-                            <option value="Female">여</option>
-		        </select>
+                <div>
+      <input type="radio" id="sex" name="sex" value="male"
+             checked />
+      <label for="male">남성</label>
+    </div>
+
+    <div>
+      <input type="radio" id="sex" name="sex" value="female" />
+      <label for="femakle">여성</label>
+    </div>
             <p>생년월일</p>
                 <input
                             id = "age"
@@ -270,7 +396,7 @@ const Join = () => {
                             <div className="form-btn">
                 <button
                 type = "submit"
-                onClick={onJoin}
+                onClick={onCreate}
                 >생성</button>
                 <button
                 type= "reset"
