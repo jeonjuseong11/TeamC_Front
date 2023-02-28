0456e63363c49bb37612915f8d5ebb6b77056762
WePung <akgre5@naver.com>
2023-02-28 19:32:37 +0900

    edit - App.js, Login.js
    오류 수정

--- src/App.js
+++ src/App.js
@@ -6,19 +6,18 @@ import Home from "./pages/Home";
 
 function App() {
   const [isLogin, setIsLogin] = useState(false);
-  const User = { id: "1@gmail.com", pw: "A!11111111" };
-  // return isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />;
+
+  // return 
   return (
-    <div>
-      <Routes>
-        <Route exact path="*" element={<Home id={User.id} />} />
-        <Route
-          exact
-          path="/login"
-          element={<Login id={User.id} pw={User.pw} />}
-        />
-      </Routes>
-    </div>
+    <Login />
+    // isLogin ? <Home /> : <Login setIsLogin={setIsLogin} />
+    // <div>
+    //   <Routes>
+    //     <Route exact path="*" element={<Home />} />
+    //     <Route exact path="/login" element={<Login/>}
+    //     />
+    //   </Routes>
+    // </div>
   );
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from "react";
+import "./Login.css";
+import Join from "./Join";
 
-const Login = ({ setIsLogin, id, pw }) => {
-  const [userId, setUserId] = useState("");
-  const [userPw, setUserPw] = useState("");
+const User = { id: "1@gmail.com", pw: "A!11111111" };
+
+const Login = ({ setIsLogin}) => {
+  const [isJoin, setIsJoin] = useState(false);
+
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
 
   const [idValid, setIdValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
   const [notAllow, setNotAllow] = useState(true);
 
   const handleId = (e) => {
-    setUserId(e.target.value);
+    setId(e.target.value);
     const regex =
       /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
     if (regex.test(id)) {
@@ -20,7 +26,7 @@ const Login = ({ setIsLogin, id, pw }) => {
   };
 
   const handlePw = (e) => {
-    setUserPw(e.target.value);
+    setPw(e.target.value);
     const regex =
       /^(?=.*[a-zA-z])(?=.*[0-9])(?=.*[$`~!@$!%*#^?&\\(\\)\-_=+])(?!.*[^a-zA-z0-9$`~!@$!%*#^?&\\(\\)\-_=+]).{8,20}$/;
     if (regex.test(pw)) {
@@ -39,58 +45,68 @@ const Login = ({ setIsLogin, id, pw }) => {
   }, [idValid, pwValid]);
 
   const onClickConfirmButton = () => {
-    if (userId === id && pw === pw) {
+    if (id === User.id && pw === User.pw) {
       alert("로그인 성공");
       setIsLogin(true);
-    } else if (userId !== id && userPw === pw) {
+    } else if (id !== User.id && pw === User.pw) {
       alert("아이디를 확인해주세요");
-    } else if (userId === id && userPw !== pw) {
+    } else if (id === User.id && pw !== User.pw) {
       alert("패스워드를 확인해주세요");
     } else {
       alert("가입자 정보가 없습니다.");
     }
   };
 
-  return (
+  const onJoin = () => {
+    setIsJoin(true);
+  }
+
+  return isJoin ? <Join 
+  setIsJoin = {setIsJoin}
+  /> : (
     <div className="Page">
       <div className="title">logo부분</div>
-
-      <div className="contentWrap">
-        <div className="id">
-          <input
-            type="text"
-            value={id}
-            onChange={handleId}
-            placeholder="아이디를 입력해주세요"
-          ></input>
-          <div className="errMessgeId" onChange={handleId}>
-            {!idValid && id.length > 0 && (
-              <div>올바른 아이디를 입력해주세요</div>
+        <div className="contentWrap">
+          <div className="id">
+            <p>아이디</p>
+            <input
+              type="text"
+              value={id}
+              onChange={handleId}
+              placeholder="아이디를 입력해주세요"
+            ></input>
+            <div className="errMessgeId" onChange={handleId}>
+              {!idValid && id.length > 0 && (
+                <p style={idValid ? {color:"green"} : {color:"red"}}>올바른 아이디를 입력해주세요</p>
+              )}
+            </div>
+          </div>
+          <div className="password">
+          <p>비밀번호</p>
+            <input
+              type="password"
+              value={pw}
+              onChange={handlePw}
+              placeholder="패스워드를 입력해주세요"
+            ></input>
+          </div>
+          <div className="errMessgePassword">
+            {!pwValid && pw.length > 0 && (
+              <p style={pwValid ? {color:"green"} : {color:"red"}}>올바른 패스워드를 입력해주세요</p>
             )}
           </div>
+          <div className="btnWrap">
+          <button
+            className="loginBtn"
+            disabled={notAllow}
+            onClick={onClickConfirmButton}
+          >
+            로그인
+          </button>
+          <button
+          onClick = {onJoin}
+          >회원가입</button>
         </div>
-        <div className="password">
-          <input
-            type="password"
-            value={pw}
-            onChange={handlePw}
-            placeholder="패스워드를 입력해주세요"
-          ></input>
-        </div>
-        <div className="errMessgePassword">
-          {!pwValid && pw.length > 0 && (
-            <div>올바른 패스워드를 입력해주세요</div>
-          )}
-        </div>
-      </div>
-      <div className="btnWrap">
-        <button
-          className="loginBtn"
-          disabled={notAllow}
-          onClick={onClickConfirmButton}
-        >
-          로그인
-        </button>
       </div>
     </div>
   );
