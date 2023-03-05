61c46ff88eff78d17d79c4332af3cd08307fffce
WePung <akgre5@naver.com>
2023-03-06 00:42:32 +0900

    Edit - ProfilePage, Del - MyBoarder, Add - img files
    프로필페이지 수정 및 MyBoard폴더 삭제, 이미지 파일 추가

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,8 +1,14 @@
 import React from "react";
 import Sidebar from "../../components/Sidebar/Sidebar";
 import Top from "../../components/Top/Top";
-import MyBoardList from "../../components/MyBoard/MyBoardList"
 import profileImg from "../../assets/profile.png";
+import facebookImg from "../../assets/facebook.png";
+import instagramImg from "../../assets/instagram.png";
+import githubImg from "../../assets/github.png";
+import idImg from "../../assets/id.png"
+import genderImg from "../../assets/gender.png"
+import emailImg from "../../assets/email.png"
+import calendarImg from "../../assets/calendar.png"
 import style from "./Profile.module.css";
 const Profile = ({ menus, User, setIsLogin }) => {
   return (
@@ -18,27 +24,34 @@ const Profile = ({ menus, User, setIsLogin }) => {
               </li>
               <li>{User.name}</li>
             </ul>
+            <ul className={style.AboutMe}>
+              <li className={style.UserImformationTitle}><h2>About Me</h2></li>
+              <li><p>자신에 대해 설명해주세요 자신에 대해 설명해주세요 자신에 대해 설명해주세요</p></li>
+            </ul>
           </div>
           <div className={style.UserImformation}>
             <ul>
-              <li>아이디 : {User.id}</li>
-              <li>이메일 : {User.email}</li>
-              <li>성별 : {User.sex}</li>
-              <li>생년월일 : {User.age}</li>
+              <li className={style.UserImformationTitle}><h2>Imformation</h2></li>
+              <li><img className ={style.IdIcon} src = {idImg} />Id : {User.id}</li>
+              <li><img className ={style.EmailIcon} src = {emailImg} />Email : {User.email}</li>
+              <li><img className ={style.GenderIcon} src = {genderImg} />Gender : {User.sex}</li>
+              <li><img className ={style.CalendarIcon} src = {calendarImg} />Birth : {User.age}</li>
+            </ul>
+            <ul className={style.Sns}>
+              <li><a href = "#"><img className={style.SnsIcon} src={facebookImg} /></a></li>
+              <li><a href = "#"><img className={style.SnsIcon} src={instagramImg} /></a></li>
+              <li><a href = "#"><img className={style.SnsIcon} src={githubImg} /></a></li>
             </ul>
           </div>
         </div>
-        {/* <div className={style.UserSns}>
-        // sns주소 바로가기버튼 추후 추가 예정
-      </div> */}
-        <div className={style.MyPostList}>
+        {/* <div className={style.MyPostList}>
           <div className={style.MyCreatedList}>
             <MyBoardList />
           </div>
-          {/* <div className={style.CommentedPostList}>
-        // 댓글쓴 글 표시창 추후 추가 예정
+          <div className={style.CommentedPostList}>
+          // 댓글쓴 글 표시창 추후 추가 예정
+          </div>
         </div> */}
-        </div>
       </div>
     </div>
   );
