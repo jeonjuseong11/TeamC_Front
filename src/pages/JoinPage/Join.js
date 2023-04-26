f7e0959edad76ec534ab704ad85baf8664d9ef95
WePung <akgre5@naver.com>
2023-04-27 00:04:07 +0900

    edit : Join.js, join.css
    joinPage 수정

--- src/pages/JoinPage/Join.js
+++ src/pages/JoinPage/Join.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Join.css';
 import axios from 'axios';
-import useHandler from '../../hooks/useHandler';
+
 
 const Join = () => {
   // 계정 배열 초기값
@@ -15,6 +15,7 @@ const Join = () => {
     sex: '',
     age: 0,
   });
+
   // 유효성 검사를 위한 상태
   const [idValid, setIdValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
@@ -37,26 +38,26 @@ const Join = () => {
   const handleId = (e) => {
     const inputIdValue = e.target.value;
     setAccount({ ...account, id: inputIdValue });
-    // const regex =
-    //   /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
-    // //이메일 형식이 만족하지 않으면 setIdVaild가 false
-    // if (regex.test(account.id)) {
-    //   setIdValid(true);
-    // } else {
-    //   setIdValid(false);
-    // }
+    const regex =
+      /^[A-Za-z]{4,25}$/;
+    //이메일 형식이 만족하지 않으면 setIdVaild가 false
+    if (regex.test(account.id)) {
+      setIdValid(true);
+    } else {
+      setIdValid(false);
+    }
   };
 
   // 비밀번호 제한
   const handlePw = (e) => {
     const inputPwValue = e.target.value;
     setAccount({ ...account, pw: inputPwValue });
-    // const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,12}$/;
-    // if (regex.test(account.pw)) {
-    //   setPwValid(true);
-    // } else {
-    //   setPwValid(false);
-    // }
+    const regex = /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,12}$/;
+    if (regex.test(account.pw)) {
+      setPwValid(true);
+    } else {
+      setPwValid(false);
+    }
   };
 
   // 비밀번호 확인 제한
@@ -80,13 +81,13 @@ const Join = () => {
   const handleEmail = (e) => {
     const inputEmailValue = e.target.value;
     setAccount({ ...account, email: inputEmailValue });
-    // const regex =
-    //   /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
-    // if (regex.test(account.email)) {
-    //   setEmailValid(true);
-    // } else {
-    //   setEmailValid(false);
-    // }
+    const regex =
+      /^[A-Za-z0-9_]+[A-Za-z0-9]*[@]{1}[A-Za-z0-9]+[A-Za-z0-9]*[.]{1}[A-Za-z]{1,3}$/;
+    if (regex.test(account.email)) {
+      setEmailValid(true);
+    } else {
+      setEmailValid(false);
+    }
   };
 
   // 성별 설정
@@ -139,137 +140,104 @@ const Join = () => {
   return (
     <div className="Join">
       <div className="container">
-        <h1>회원가입</h1>
-        <label>아이디</label>
-        <input
-          id="id"
-          name="id"
-          type="text"
-          value={account.id}
-          placeholder="아이디를 적어주세요"
-          onChange={handleId}
-          // style={
-          //   account.id.length === 0
-          //     ? { border: "solid 1px black" }
-          //     : idValid
-          //     ? { border: "solid 1px green" }
-          //     : { border: "solid 1px red" }
-          // }
-        />
-        <label>패스워드</label>
-        <input
-          id="pw"
-          name="pw"
-          type="password"
-          maxLength="12"
-          value={account.pw}
-          placeholder="영대소문자, 특수기함 포함 8자 이상"
-          onChange={handlePw}
-          // style={
-          //   account.pw.length === 0
-          //     ? { border: "solid 1px black" }
-          //     : pwValid
-          //     ? { border: "solid 1px green" }
-          //     : { border: "solid 1px red" }
-          // }
-        />
-        <label>패스워드 재확인</label>
-        <input
-          id="pwRewind"
-          name="pwRewind"
-          type="password"
-          maxLength="12"
-          value={account.pwRewind}
-          onChange={handlePwRewind}
-          // style={
-          //   account.pwRewind.length === 0
-          //     ? { border: "solid 1px black" }
-          //     : pwRewindValid
-          //     ? { border: "solid 1px green" }
-          //     : { border: "solid 1px red" }
-          // }
-        />
-        <label>이름</label>
-        <input
-          id="userName"
-          name="userName"
-          type="text"
-          value={account.name}
-          placeholder="이름을 적어주세요"
-          onChange={handleName}
-          maxLength="5"
-          // style={
-          //   account.name.length === 0
-          //     ? { border: "solid 1px black" }
-          //     : nameValid
-          //     ? { border: "solid 1px green" }
-          //     : { border: "solid 1px red" }
-          // }
-        />
-        <label>이메일</label>
-        <input
-          id="email"
-          name="email"
-          type="text"
-          value={account.email}
-          placeholder="이메일을 적어주세요"
-          onChange={handleEmail}
-          // style={
-          //   account.email.length === 0
-          //     ? { border: "solid 1px black" }
-          //     : emailValid
-          //     ? { border: "solid 1px green" }
-          //     : { border: "solid 1px red" }
-          // }
-        />
-        <div>
-          <label>성별</label>
-          <div className="radio-btn">
-            <label className="female-radio">
+        <form>
+          <h2>회원가입</h2>
+          <label>아이디</label>
+          <input
+            id="id"
+            name="id"
+            type="text"
+            value={account.id}
+            placeholder="아이디를 적어주세요"
+            onChange={handleId}
+          />
+          <label>패스워드</label>
+          <input
+            id="pw"
+            name="pw"
+            type="password"
+            maxLength="12"
+            value={account.pw}
+            placeholder="영대소문자, 특수기함 포함 8자 이상"
+            onChange={handlePw}
+          />
+          <label>패스워드 재확인</label>
+          <input
+            id="pwRewind"
+            name="pwRewind"
+            type="password"
+            maxLength="12"
+            value={account.pwRewind}
+            onChange={handlePwRewind}
+          />
+          <label>이름</label>
+          <input
+            id="userName"
+            name="userName"
+            type="text"
+            value={account.name}
+            placeholder="이름을 적어주세요"
+            onChange={handleName}
+            maxLength="5"
+          />
+          <label>이메일</label>
+          <input
+            id="email"
+            name="email"
+            type="text"
+            value={account.email}
+            placeholder="이메일을 적어주세요"
+            onChange={handleEmail}
+          />
+          <div className="lastWrap">
+            <div className="sexWrapper">
+              <label>성별</label>
+
+                <div className="radioBtnWrapper">
+                  <div className="femaleRadio">
+                    <input
+                      type="radio"
+                      name="sex"
+                      id="sex"
+                      value="female"
+                      onClick={onSexClick}
+                    />
+                    <label>여성</label>
+                  </div>
+                  <div className="maleRadio">
+                    <input
+                      type="radio"
+                      name="sex"
+                      id="sex"
+                      value="male"
+                      onClick={onSexClick}
+                    />
+                    <label>남성</label>
+                  </div>
+                </div>
+
+            </div>
+            <div className="age">
+              <label>생년월일</label>
               <input
-                type="radio"
-                name="sex"
-                id="sex"
-                value="female"
-                onClick={onSexClick}
+                id="age"
+                name="age"
+                type="number"
+                // placeholder="생년월일 6자리를 입력해주세요"
+                value={account.age}
+                onChange={handleAge}
               />
-              여성
-            </label>
-            <label className="male-radio">
-              <input
-                type="radio"
-                name="sex"
-                id="sex"
-                value="male"
-                onClick={onSexClick}
-              />
-              남성
-            </label>
+            </div>
           </div>
-        </div>
-        <label>생년월일</label>
-        <input
-          id="age"
-          name="age"
-          type="number"
-          value={account.age}
-          onChange={handleAge}
-          // style={
-          //   account.age === 0
-          //     ? { border: "solid 1px black" }
-          //     : ageValid
-          //     ? { border: "solid 1px green" }
-          //     : { border: "solid 1px red" }
-          // }
-        />
-        <div className="form-btn">
-          <button className="create-btn" type="submit" onClick={joinFunc}>
-            생성
-          </button>
-          <button className="back-btn" onClick={backToList}>
-            취소
-          </button>
-        </div>
+          <div className="formBtnWrapper">
+            <button className="createBtn" type="submit" onClick={joinFunc}>
+              가입하기
+            </button>
+            <button className="backBtn" onClick={backToList}>
+              취소
+            </button>
+          </div>
+          </form>
       </div>
     </div>
   );
